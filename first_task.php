e3f86e69c1447c4101c2bf3261b81e5dfe672b0b
Kaiyrkhan <serik@spark.kz>
2023-10-09 16:50:33 +0600

    [first_task] завершена первая задача

--- first_task.php
+++ first_task.php
@@ -1,6 +1,19 @@
 <?php
 
+declare(strict_types=1);
 
 class MySingleton {
+    private static MySingleton $existedInstance;
+    private function __construct()
+    {
+    }
 
+    public static function getInstance(): self
+    {
+        if (!self::$existedInstance) {
+            self::$existedInstance = new self();
+        }
+
+        return self::$existedInstance;
+    }
 }
